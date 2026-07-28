1fe533269566b8451e9429a678927a802e8a61b6
Rezwan <rayjan4567@gmail.com>
2026-07-28 20:53:08 +0600

    28-07-26

--- lib/module11/Grid.dart
+++ lib/module11/Grid.dart
@@ -8,24 +8,44 @@ class GridV extends StatelessWidget {
       appBar: AppBar(
         title: Text("Grid"),
       ),
-      body: GridView.count(
-        crossAxisCount: 3,
-
-          children: [
-            Container(
-              color: Colors.red,
-            ),
-            Container(
-              color: Colors.green,
-            ),
-            Container(
-              color: Colors.blue,
-            ),
-            Container(
-              color: Colors.yellow,
-            )
-          ],
-      ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: 20,
+          itemBuilder: (context,index){
+            return Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  Text('cash out')
+                ],
+              ),
+            );
+          }
+      )
+      // GridView.count(
+      //   crossAxisCount: 3,
+      //     crossAxisSpacing: 10,
+      //     mainAxisSpacing: 10,
+      //     children: [
+      //       Container(
+      //         color: Colors.red,
+      //       ),
+      //       Container(
+      //         color: Colors.green,
+      //       ),
+      //       Container(
+      //         color: Colors.blue,
+      //       ),
+      //       Container(
+      //         color: Colors.yellow,
+      //       )
+      //     ],
+      // ),
     );
   }
 }
